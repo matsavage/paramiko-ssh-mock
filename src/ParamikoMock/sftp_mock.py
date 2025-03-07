2d64a3eb440f0d8a3c78e5305012b4e557904db5
Paulo Vitor Magacho <139177232+pvmagacho-nde@users.noreply.github.com>
2025-03-06 21:14:35 -0300

    Update sftp_mock.py

--- src/ParamikoMock/sftp_mock.py
+++ src/ParamikoMock/sftp_mock.py
@@ -1,3 +1,7 @@
+import time
+import os
+from paramiko import SFTPAttributes
+
 class SFTPFileMock():
     written = []
     file_content = None
@@ -19,3 +23,28 @@ class SFTPClientMock():
     
     def close(self):
         pass
+
+    def put(self, localpath, remotepath, callback=None, confirm=True):
+        # Creating a fake os.stat_result object
+        size = len(self.sftp_file_mock.file_content)
+        fake_stat = os.stat_result((
+            33206,   # st_mode (file mode)
+            1234567, # st_ino (inode number)
+            1000,    # st_dev (device)
+            1,       # st_nlink (number of hard links)
+            1001,    # st_uid (user ID of owner)
+            1002,    # st_gid (group ID of owner)
+            size,    # st_size (size in bytes)
+            int(time.time()),  # st_atime (last access time)
+            int(time.time()),  # st_mtime (last modification time)
+            int(time.time())   # st_ctime (creation time on Windows, metadata change on Unix)
+        ))
+        if confirm:
+            s = SFTPAttributes.from_stat(fake_stat)
+            if s.st_size != size:
+                raise IOError(
+                    "size mismatch in put!  {} != {}".format(s.st_size, size)
+                )
+        else:
+            s = SFTPAttributes()
+        return s
